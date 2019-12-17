8e564a32c4daf03c6e7abf3a75480a55787594e5
SuperRich <r-hunter@live.co.uk>
2019-12-17 21:03:57 +0000

    .NET Core Crud done

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -10,7 +10,7 @@ class App extends Component {
     
     componentDidMount() {
         axios.get('http://localhost:5000/api/values').then(r => 
-        {Tnode
+        {
             this.setState({
             values: r.data
         })});
