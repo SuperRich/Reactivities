bafea30a082ae5887c0b9c4cd17484e0e1c0835f
SuperRich <r-hunter@live.co.uk>
2019-12-18 20:06:11 +0000

    React hooks Activity.ts

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,37 @@
+﻿import {IActivity} from "../models/Activity";
+
+﻿﻿import React, {useState,useEffect} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+
+
+const App = () => {
+    
+    //first argument is the state, second argument is the function call to set the state
+    //initial state is an empty array
+    const [activities, setActivities] = useState<IActivity[]>([])
+
+    useEffect(() => {
+        axios.get<IActivity[]>('http://localhost:5000/api/activities').then(r =>
+            {
+                setActivities(r.data)
+                //empty second param makes sure we only get the activities once
+            })},[]);
+    
+        return (
+            <div>
+                <Header as='h2'>
+                    <Icon name='users' />
+                    <Header.Content>Reactivities</Header.Content>
+                </Header>
+                <List>
+                    {activities.map((activity) => (
+                        <List.Item key={activity.id}>{activity.title}</List.Item>
+                    ))}
+                </List>
+            </div>
+        );
+    }
+    
+
+export default App;
