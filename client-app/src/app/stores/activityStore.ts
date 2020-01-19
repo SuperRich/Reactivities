8ee43aec9937d2c02176ad964d8067557debc84a
SuperRich <r-hunter@live.co.uk>
2020-01-19 00:30:48 +0000

    Styling with semantic ui react done - looks good now

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,8 +1,7 @@
-﻿import {observable, action, computed, configure, runInAction} from "mobx";
+﻿import {action, computed, configure, observable, runInAction} from "mobx";
 import {createContext, SyntheticEvent} from "react";
 import {IActivity} from "../models/Activity";
 import agent from "../api/agent";
-
 
 configure({enforceActions: 'always'});
 
@@ -14,7 +13,19 @@ class ActivityStore {
    @observable target = '';
    
    @computed get activitiesByDate() {
-      return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+      return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()))
+   }
+   groupActivitiesByDate(activities :IActivity[]) {
+      const sortedActivities = activities.sort(
+          (a, b) => Date.parse(a.date) - Date.parse(b.date)
+      );
+      
+      return Object.entries(sortedActivities.reduce((activities, activity) => 
+      {
+         const date = activity.date.split('T')[0];
+         activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+         return activities
+      }, {} as {[key: string] : IActivity[]}));
    }
    
    @action loadActivities = async () => {
